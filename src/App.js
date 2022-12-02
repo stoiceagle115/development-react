7bbb5482da68f84984893e10eefddc9253dc47dd
stoiceagle115 <114535656+stoiceagle115@users.noreply.github.com>
2022-12-02 17:42:22 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -31,10 +31,7 @@ import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
 import BakeryItem from "./components/BakeryItem.js";
 
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
